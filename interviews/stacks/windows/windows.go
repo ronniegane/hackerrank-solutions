58c3a9ed9721811e63a314c9aff55601aaf4e3e5
Ronnie Gane <dev@ronniegane.kiwi>
2019-02-14 16:19:54 +1300

    added interviews/stacks/windows

---
+++ interviews/stacks/windows/windows.go
@@ -0,0 +1,19 @@
+package windows
+
+/*
+https://www.hackerrank.com/challenges/min-max-riddle/problem
+
+Given an input array of length n,
+for each window length 1...n calculate the minimum value of each window
+return an array that describes the maximum value of these minimums
+for each window length
+
+seems similar to the builings problem
+*/
+
+func riddle(arr []int64) []int64 {
+
+	ret := make([]int64, len(arr))
+
+	return ret
+}
