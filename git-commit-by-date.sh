1389d39747f0cd7f31ab19a79ceaf83f7844f4a9
Ronnie Gane <dev@ronniegane.kiwi>
2021-08-30 13:17:39 +1200

    Altered commit-making script to read dates and paths from input file

--- git-commit-by-date.sh
+++ git-commit-by-date.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-IFS=$'\n'
-for res in $(find . -type d -links 2 -printf "%TY-%Tm-%TdT%TH:%TM:%.2TS %p\n" | sort -n);
+# Create the input file with the following:
+# find . -type d -links 2 -printf "%TY-%Tm-%TdT%TH:%TM:%.2TS %p\n" | sort -n > folder-modified-dates.txt
+
+while read -r res;
 do
-    echo $res;
-    date="$(echo $res | cut -d' ' -f1)";
-    filepath="$(echo $res | cut -d' ' -f2)";
+    echo "$res";
+    date="$(echo "$res" | cut -d' ' -f1)";
+    filepath="$(echo "$res" | cut -d' ' -f2)";
     if [[ $filepath == *"/."* ]]; then
       echo "dot folder, skipping";
     else
       git add "$filepath";
       GIT_COMMITTER_DATE="$date" GIT_AUTHOR_DATE="$date" git commit -m "added $(echo "$filepath" | cut -c3-)";
     fi;
-done;
+done < folder-modified-dates.txt;
